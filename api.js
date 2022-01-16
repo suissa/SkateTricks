7cea3cfef0e3e95be6128e7c4a4a8aa4e5ac7169
Suissinha <jnascimento@gmail.com>
2022-01-16 18:21:09 -0300

    api

---
+++ api.js
@@ -0,0 +1,20 @@
+const Model = require("./model.tricks")
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/tricks', async (req, res) => {
+  const result = await Model.find({})
+  res.json(result)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
